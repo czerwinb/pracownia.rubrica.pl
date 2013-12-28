b1950bebfda87f093276e6ab6dff48e7d76637f0
czerwinb <bczerwinski@bsnetworks.pl>
2013-12-28 21:28:57 +0100

    Initial Grunt configuration

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Load Grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+    });
+}
